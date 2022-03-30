a3f13be8d2d3b6b34f3f80ff52d93ce1ea5796cd
gerreiracunha <gferreiracunha@gmail.com>
2022-03-30 14:55:17 -0300

    ajuste para instalação.

--- aco.py
+++ aco.py
@@ -4,11 +4,9 @@ import tsplib95
 import time
 import random
 import click
-from os import listdir
-from os.path import isfile, join
 
 from acopy import ant, solvers, plugins, utils
-from numpy.lib import math
+
 
 
 class add_demand(acopy.solvers.SolverPlugin):
@@ -21,43 +19,45 @@ class add_demand(acopy.solvers.SolverPlugin):
         antaux = state.colony.get_ants(self.delta)
 #        state.ants.append(antaux)
 
-
-path = "dataset/A-n32-k5.vrp"
+path = "dataset/E-n22-k4.vrp"
 
 problem = tsplib95.load(path)
 
 G = problem.get_graph()
 
+alpha=2
+beta=5
+capacidade_feromonio=problem.capacity
+quantidade_formigas=4
+fator_evaporacao_feromonio=0.8
+numero_interacao=1000
+
 seed = seed or str(hash(time.time()))
 click.echo(f'SEED={seed}')
 random.seed(seed)
 
-colony = ant.Colony(alpha=2, beta=5)
-solver = solvers.Solver(rho=0.8, q=problem.capacity, top=5)
+colony = ant.Colony(alpha=alpha, beta=beta)
+solver = solvers.Solver(rho=fator_evaporacao_feromonio, q=capacidade_feromonio, top=quantidade_formigas)
 
 click.echo(solver)
 
 printout = plugins.Printout()
-click.echo(f'Registering plugin: {printout}')
 solver.add_plugin(printout)
 
 timer = plugins.Timer()
-click.echo(f'Registering plugin: {timer}')
 solver.add_plugin(timer)
 
 plugin = plugins.Threshold(threshold=784)
-click.echo(f'Registering plugin: {plugin}')
-solver.add_plugin(plugin)
+#solver.add_plugin(plugin)
 
 recorder = plugins.StatsRecorder()
-click.echo(f'Registering plugin: {recorder}')
 solver.add_plugin(recorder)
 
 demand = add_demand(1)
 solver.add_plugin(demand)
-click.echo(f'Registering plugin: {demand}')
 
-solver.solve(G, colony, gen_size=1, limit=50)
+
+result=solver.solve(G, colony, gen_size=quantidade_formigas, limit=numero_interacao)
 
 click.echo(timer.get_report())
 if recorder:
