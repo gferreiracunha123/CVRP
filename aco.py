1f809149f2effe025a0db65a0df32ecf77e75039
gerreiracunha <gferreiracunha@gmail.com>
2022-04-13 03:23:55 -0300

    implementação CVRP

--- aco.py
+++ aco.py
@@ -1,50 +1,38 @@
+import csv
 import math
 from os import listdir
 from os.path import isfile, join
 
-import seed as seed
 import tsplib95
-import time
-import random
-
-
-from acopy import ant, solvers, plugins, utils
-
-
-
-seed = seed or str(hash(time.time()))
+from acopy import ant, solvers, plugins
 
 
 def dist(a, b):
     (x1, y1), (x2, y2) = a, b
     return math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
 
+
 def procurarota(alpha, beta, capacidade_feromonio, quantidade_formigas, fator_evaporacao_feromonio, numero_interacao,
-                seed,rotas_ja_visitadas,problem):
-  #  click.echo(f'SEED={seed}')
-    random.seed(seed)
-    if(len(problem.node_coords)!=1):
-        listremover=dict(problem.node_coords)
+                rotas_ja_visitadas, problem):
+    #  click.echo(f'SEED={seed}')
+
+    if (len(problem.node_coords) != 1):
+        listremover = dict(problem.node_coords)
         for rotas in rotas_ja_visitadas:
-            if(rotas!=1):
+            if (rotas != 1):
                 listremover.pop(rotas)
 
-
-
-        problem.node_coords=listremover
+        problem.node_coords = listremover
 
     G = problem.get_graph()
-
-
-
 
     colony = ant.Colony(alpha=alpha, beta=beta)
     solver = solvers.Solver(rho=fator_evaporacao_feromonio, q=capacidade_feromonio)
 
-   # click.echo(solver)
+    # click.echo(solver)
 
     printout = plugins.Printout()
-   # solver.add_plugin(printout)
+    solver.add_plugin(printout)
 
     timer = plugins.Timer()
     solver.add_plugin(timer)
@@ -52,13 +40,9 @@ def procurarota(alpha, beta, capacidade_feromonio, quantidade_formigas, fator_ev
     recorder = plugins.StatsRecorder()
     solver.add_plugin(recorder)
 
-    #demand = add_demand(1)
-   # solver.add_plugin(demand)
+    result = solver.solve(G, colony, gen_size=quantidade_formigas, limit=numero_interacao)
 
-
-    result = solver.solve(G, colony, gen_size=5, limit=numero_interacao)
-
- #   click.echo(timer.get_report())
+    # timer.get_report()
     return result
     # if recorder:
     # plotter = utils.plot.Plotter(recorder.stats)
@@ -66,52 +50,55 @@ def procurarota(alpha, beta, capacidade_feromonio, quantidade_formigas, fator_ev
 
 
 def carga(problem):
-
-    dict_solucao=dict({})
-    list_dict_solucao=[]
+    dict_solucao = dict({})
+    list_dict_solucao = []
     alpha = 2
     beta = 5
     capacidade_feromonio = problem.capacity
-    quantidade_formigas = int(str(problem.comment).split(":")[1].split(",")[0])
+    quantidade_formigas = 5
+    try:
+        quantidade_formigas = int(str(problem.comment).split(":")[1].split(",")[0])
+    except:
+        print("Quantidade formigas default")
     fator_evaporacao_feromonio = 0.8
-    numero_interacao = 50
+    numero_interacao = 100
 
     rotas = procurarota(alpha, beta, capacidade_feromonio, quantidade_formigas, fator_evaporacao_feromonio,
-                           numero_interacao,
-                           seed,[],problem)
+                        numero_interacao, [], problem)
 
-    rotas=[int(x) for x in  str(rotas).split("\t")[1].split(",")]
+    rotas = [int(x) for x in str(rotas).split("\t")[1].split(",")]
 
-    capacidade=0
-    veiculo=0
-    capacidadetotal=0
+    capacidade = 0
+    veiculo = 0
+    capacidadetotal = 0
     solucao = []
 
-    totalallrotascapaciti=0
+    totalallrotascapaciti = 0
 
     for demandcount in problem.demands:
-        totalallrotascapaciti=problem.demands[demandcount]+totalallrotascapaciti
+        totalallrotascapaciti = problem.demands[demandcount] + totalallrotascapaciti
 
-
-
-    while capacidadetotal <totalallrotascapaciti:
+    while capacidadetotal < totalallrotascapaciti:
         index = 0
         distancia = 0
 
-
-        rotas=(rotas)
+        rotas = (rotas)
         for demand in rotas:
-            capacidade=problem.demands[demand]+capacidade
-            if(capacidade<=problem.capacity):
+            capacidade = problem.demands[demand] + capacidade
+            if capacidade <= problem.capacity:
                 solucao.append(demand)
-                if(index+1<=len(rotas)-1):
-                     distancia=dist(problem.node_coords[rotas[index]],problem.node_coords[rotas[index+1]])+distancia
+                if index + 1 <= len(rotas) - 1:
+                    distancia = dist(problem.node_coords[rotas[index]],
+                                     problem.node_coords[rotas[index + 1]]) + distancia
+                else:
+                    distancia = dist(problem.node_coords[rotas[index]],
+                                     problem.node_coords[rotas[0]]) + distancia
             else:
-                capacidade = capacidade-problem.demands[demand]
+                capacidade = capacidade - problem.demands[demand]
                 break
-            index=index+1
+            index = index + 1
 
-        #print("veiculo-", veiculo, (solucao), " capacidade:", capacidade, " distancia ", distancia)
+        # print("veiculo-", veiculo, (solucao), " capacidade:", capacidade, " distancia ", distancia)
         dict_solucao.update({"veiculo": veiculo})
         dict_solucao.update({"capacidade": capacidade})
         dict_solucao.update({"rota": solucao})
@@ -120,36 +107,31 @@ def carga(problem):
         list_dict_solucao.append(dict_solucao)
         dict_solucao = dict({})
 
-        if capacidadetotal <totalallrotascapaciti:
+        if capacidadetotal < totalallrotascapaciti:
             try:
                 rotas = procurarota(alpha, beta, capacidade_feromonio, quantidade_formigas, fator_evaporacao_feromonio,
-                            numero_interacao,
-                            seed,solucao,problem)
+                                    numero_interacao, solucao, problem)
             except:
                 print("Carga distribuida")
                 return list_dict_solucao
 
-        rotas = [int(x) for x in  str(rotas).split("\t")[1].split(",")]
+        rotas = [int(x) for x in str(rotas).split("\t")[1].split(",")]
 
-        veiculo=veiculo+1
-        capacidadetotal=capacidade+capacidadetotal
+        veiculo = veiculo + 1
+        capacidadetotal = capacidade + capacidadetotal
         capacidade = 0
         solucao = []
 
     return list_dict_solucao
 
 
-
-
-
-
 def main():
-
-
+    open('dataset/analise/analise_Ant.csv', 'w', newline='', encoding='utf-8')
     raiz = 'dataset'
     files = [f for f in listdir(raiz) if isfile(join(raiz, f))]
+    index = 0
     for file in files:
-       # if(file=="E-n22-k4.vrp"):
+        if file != "Loggi-n501-k24.vrp":
             total_carga = 0
             total_distancia = 0
             problem = tsplib95.load(raiz + "/" + file)
@@ -157,11 +139,29 @@ def main():
             print(problem.name)
             for veiculo in list_veiculos:
                 print(veiculo)
-                total_carga=total_carga+veiculo.get("capacidade")
+                total_carga = total_carga + veiculo.get("capacidade")
                 total_distancia = total_distancia + veiculo.get("distancia")
-            print("Capacidade Total ",total_carga,"Distancia Rotas Total",total_distancia)
-            break
+            print('Capacidade Total %s "Distancia Rotas Total %s' % (total_carga, total_distancia))
+            cria_csv(index == 0, file, len(list_veiculos), total_carga, total_distancia, 0)
+            index = index + 1
 
+
+    exit(1)
+
+
+def cria_csv(inicio, nome, qnt_veiculo, total_capacidade, total_distancia, interacao):
+    f = open('dataset/analise/analise_Ant.csv', 'a', newline='', encoding='utf-8')
+
+    # 2. cria o objeto de gravação
+    w = csv.writer(f)
+
+    # 3. grava as linhas
+    if inicio:
+        w.writerow(["Nome", "qnt Veiculos ", "Total Capacidade", "Total Distancia", "qnt Interacao","Tempo"])
+    w.writerow([nome, qnt_veiculo, total_capacidade, total_distancia, interacao, 0])
+
+    # Recomendado: feche o arquivo
+    f.close()
 
 
 if __name__ == '__main__':
--- ortoolsSolution.py
+++ ortoolsSolution.py
@@ -1,49 +1,55 @@
+import csv
+from os import listdir
+from os.path import isfile, join
+
 import tsplib95
 from numpy.lib import math
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 
+
 def distance(lat1, long1, lat2, long2):
-    return euclidean([lat1,long1],[lat2,long2])
+    return dist([lat1, long1], [lat2, long2])
 
-def create_data_model():
+
+def create_data_model(path):
     """Stores the data for the problem."""
-    path = "dataset/E-n22-k4.vrp"
-
     dataset = tsplib95.load(path)
 
-
-    distacia_x_y=list(dataset.node_coords.values())
-    distacia=[]
+    distacia_x_y = list(dataset.node_coords.values())
+    distacia = []
 
     index = 0
-    #cria matriz de distancia
+    # cria matriz de distancia
     for x in distacia_x_y:
-        indey: int  = int(0)
-        distaciaux=[]
+        indey = int(0)
+        distaciaux = []
         for y in distacia_x_y:
-            distaciaux.append(euclidean(x,y))
-            indey=indey+1
+            distaciaux.append(dist(x, y))
+            indey = indey + 1
         distacia.append(distaciaux)
-        index=index+1
+        index = index + 1
 
     data = {}
-    data['distance_matrix'] =distacia
-        #calculate_distance_matrix( list(dataset.node_coords.values()))
-    data['demands'] =list(dataset.demands.values())
-        #list(dataset.demands.values())
-    data['num_vehicles'] = 4
-    data['vehicle_capacities'] = [6000] *  data['num_vehicles']
+    data['distance_matrix'] = distacia
+    # calculate_distance_matrix( list(dataset.node_coords.values()))
+    data['demands'] = list(dataset.demands.values())
+    # list(dataset.demands.values())
+    quantidade_formigas = 5
+    try:
+        quantidade_formigas = int(str(dataset.comment).split(":")[1].split(",")[0])
+    except:
+        print("Quantidade formigas default")
+
+    data['num_vehicles'] = quantidade_formigas
+    data['vehicle_capacities'] = [dataset.capacity] * data['num_vehicles']
     data['depot'] = 0
     return data
 
 
-def print_solution(data, manager, routing, solution):
-    path = "dataset/E-n22-k4.vrp"
-
-    dataset = tsplib95.load(path)
-
-    distacia_x_y = list(dataset.node_coords.values())
+def print_solution(inicio, file, path, data, manager, routing, solution):
+    dict_solucao = dict({})
+    list_dict_solucao = []
     """Prints solution on console."""
     print(f'Objective: {solution.ObjectiveValue()}')
     total_distance = 0
@@ -59,16 +65,15 @@ def print_solution(data, manager, routing, solution):
             node_index = manager.IndexToNode(index)
             route_load += data['demands'][node_index]
             plan_output += ' {0} --- ({1}) -> '.format(node_index, route_load)
-            plan_output2 += ' [{0} , {1}]= {2} -> '.format(distacia_x_y[node_index][0], distacia_x_y[node_index][1],data['demands'][node_index])
+
             previous_index = index
             index = solution.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id)
 
         plan_output += ' {0} Carga ({1})\n'.format(manager.IndexToNode(index),
-                                                 route_load)
-        plan_output2 += ' [{0} , {1}]= {2} -> '.format(distacia_x_y[manager.IndexToNode(index)][0], distacia_x_y[manager.IndexToNode(index)][1],
-                                                       data['demands'][manager.IndexToNode(index)])
+                                                   route_load)
+
         plan_output += 'Distância da rota: {}m\n'.format(route_distance)
         plan_output += 'Carga da rota: {}\n'.format(route_load)
         print(plan_output)
@@ -76,74 +81,108 @@ def print_solution(data, manager, routing, solution):
 
         total_distance += route_distance
         total_load += route_load
+        dict_solucao.update({"veiculo": vehicle_id})
+        dict_solucao.update({"capacidade": route_load})
+        dict_solucao.update({"rota": route_load})
+        dict_solucao.update({"distancia": route_distance})
+        list_dict_solucao.append(dict_solucao)
+        dict_solucao = dict({})
     print('Total distance of all routes: {}m'.format(total_distance))
     print('Total load of all routes: {}'.format(total_load))
+    cria_csv(inicio, file, len(list_dict_solucao), total_load, total_distance, 0)
 
-def euclidean(a, b):
-    return math.sqrt(pow(a[1] - b[1], 2) + pow(a[0] - b[0], 2))
+
+''
+
+
+def dist(a, b):
+    (x1, y1), (x2, y2) = a, b
+    return math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+
 
 def main():
     """Solve the CVRP problem."""
     # Instantiate the data problem.
-    data = create_data_model()
+    open('dataset/analise/analise_Ortools.csv', 'w', newline='', encoding='utf-8')
+    raiz = 'dataset'
+    files = [f for f in listdir(raiz) if isfile(join(raiz, f))]
+    index = 0
+    for file in files:
+        if file != "Loggi-n501-k24.vrp":
+            path = raiz + "/" + file
+            data = create_data_model(path)
 
-    # Create the routing index manager.
-    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
-                                           data['num_vehicles'], data['depot'])
+            # Create the routing index manager.
+            manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                                   data['num_vehicles'], data['depot'])
 
-    # Create Routing Model.
-    routing = pywrapcp.RoutingModel(manager)
+            # Create Routing Model.
+            routing = pywrapcp.RoutingModel(manager)
+
+            # Create and register a transit callback.
+            def distance_callback(from_index, to_index):
+                """Returns the distance between the two nodes."""
+                # Convert from routing variable Index to distance matrix NodeIndex.
+                from_node = manager.IndexToNode(from_index)
+                to_node = manager.IndexToNode(to_index)
+                return data['distance_matrix'][from_node][to_node]
+
+            transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+            # Define cost of each arc.
+            routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+            # Add Capacity constraint.
+            def demand_callback(from_index):
+                """Returns the demand of the node."""
+                # Convert from routing variable Index to demands NodeIndex.
+                from_node = manager.IndexToNode(from_index)
+                return data['demands'][from_node]
+
+            demand_callback_index = routing.RegisterUnaryTransitCallback(
+                demand_callback)
+            routing.AddDimensionWithVehicleCapacity(
+                demand_callback_index,
+                0,  # null capacity slack
+                data['vehicle_capacities'],  # vehicle maximum capacities
+                True,  # start cumul to zero
+                'Capacity')
+
+            # Setting first solution heuristic.
+            search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+            search_parameters.first_solution_strategy = (
+                routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+            search_parameters.local_search_metaheuristic = (
+                routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+            search_parameters.time_limit.FromSeconds(1)
+
+            # Solve the problem.
+            solution = routing.SolveWithParameters(search_parameters)
+
+            # Print solution on console.
+            if solution:
+                print_solution(index == 0, file, path, data, manager, routing, solution)
+            else:
+                print("Nada")
+
+            index = index + 1
+
+    exit()
 
 
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        # Convert from routing variable Index to distance matrix NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data['distance_matrix'][from_node][to_node]
+def cria_csv(inicio, nome, qnt_veiculo, total_capacidade, total_distancia, interacao):
+    f = open('dataset/analise/analise_Ortools.csv', 'a', newline='', encoding='utf-8')
 
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    # 2. cria o objeto de gravação
+    w = csv.writer(f)
 
-    # Define cost of each arc.
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    # 3. grava as linhas
+    if inicio:
+        w.writerow(["Nome", "qnt Veiculos ", "Total Capacidade", "Total Distancia", "qnt Interacao", "Tempo"])
+    w.writerow([nome, qnt_veiculo, total_capacidade, total_distancia, interacao, 0])
 
-
-    # Add Capacity constraint.
-    def demand_callback(from_index):
-        """Returns the demand of the node."""
-        # Convert from routing variable Index to demands NodeIndex.
-        from_node = manager.IndexToNode(from_index)
-        return data['demands'][from_node]
-
-    demand_callback_index = routing.RegisterUnaryTransitCallback(
-        demand_callback)
-    routing.AddDimensionWithVehicleCapacity(
-        demand_callback_index,
-        0,  # null capacity slack
-        data['vehicle_capacities'],  # vehicle maximum capacities
-        True,  # start cumul to zero
-        'Capacity')
-
-    # Setting first solution heuristic.
-    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-    search_parameters.local_search_metaheuristic = (
-        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
-    search_parameters.time_limit.FromSeconds(1)
-
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
-
-    # Print solution on console.
-    if solution:
-        print_solution(data, manager, routing, solution)
-    else:
-        print("Nada")
-    rou=[]
-    rou.append(manager.IndexToNode(routing.Start(0)))
-    #print(rou," : ",solution.ObjectiveValue())
+    # Recomendado: feche o arquivo
+    f.close()
 
 
 if __name__ == '__main__':
